5ac29634d92c6e366ed27347f103460e9934a2d2
Artyom <artyomka13@gmail.com>
2022-10-11 19:15:40 +0300

    Initial Commit

---
+++ CardsSwiftUI/CardsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsSwiftUIApp.swift
+//  CardsSwiftUI
+//
+//  Created by Artyom Guzenko on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
